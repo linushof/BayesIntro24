3265f8525a015b1441371fdcfbad55e54f21dee1
linushof <linus.hof@tum.de>
2024-06-11 19:29:09 +0200

    updated code session 8

--- code/.R/session_08_linear_models_II.R
+++ code/.R/session_08_linear_models_II.R
@@ -4,60 +4,8 @@ pacman::p_load(tidyverse, rethinking)
 # load dark theme
 source("code/.R/dark_theme.R")
 
-# introductory example ----------------------------------------------------
 
-data(WaffleDivorce)
-d <- WaffleDivorce
-
-d$D <- standardize(d$Divorce)
-d$M <- standardize(d$Marriage)
-d$A <- standardize(d$MedianAgeMarriage)
-
-ggplot(d, aes(x = M, y = D)) + 
-  geom_point(size = 3, color = "#ff02ff") + 
-  geom_smooth(method = "lm", color = "#ff02ff") +
-  labs(x = "Marriage Rate", 
-       y = "Divorce Rate") + 
-  dark_theme()
-
-
-m1_intro <- quap(
-  alist( D ~ dnorm(mu, sigma), 
-         mu ~ a + bM * M, 
-         a ~ dnorm(0, .2), 
-         bM ~ dnorm(0, .5), 
-         sigma ~ dexp(1)), 
-  data = d
-)
-precis(m1_intro)
-
-ggplot(d, aes(x = M, y = A)) + 
-  geom_point(size = 3, color = "#ff02ff") + 
-  geom_smooth(method = "lm", color = "#ff02ff") +
-  labs(x = "Marriage Rate", 
-       y = "Median Age Marriage") + 
-  dark_theme()
-
-ggplot(d, aes(x = D, y = A)) + 
-  geom_point(size = 3, color = "#ff02ff") + 
-  geom_smooth(method = "lm", color = "#ff02ff") +
-  labs(x = "Divorce Rate", 
-       y = "Median Age Marriage") + 
-  dark_theme()
-
-m2_intro <- quap(
-  alist( D ~ dnorm(mu, sigma), 
-         mu ~ a + bM * M + bA * A, 
-         a ~ dnorm(0, .2), 
-         bM ~ dnorm(0, .5),
-         bA ~ dnorm(0, .5), 
-         sigma ~ dexp(1)), 
-  data = d
-)
-precis(m2_intro)
-
-
-# Exercie: Multiple Regression Model --------------------------------------------------------------------
+# Exercise: Multiple Regression Model --------------------------------------------------------------------
 
 ## generative simulation 
 
@@ -67,9 +15,9 @@ sim_pts <- function(FGA, FTA, hFG, hFT){
   FT <- rbinom(1, FTA, prob = hFT)
   
   # 2PA  
-  FG <- rbinom(1, FGA, prob = hFG)*2
+  FG <- rbinom(1, FGA, prob = hFG) * 2
   
-  FT + FG
+  FT + FG + rnorm(1, mean=0, sd=4)
   
 }    
 
@@ -80,6 +28,7 @@ FGA <- round(rgamma(N_games, 20, 1),0)
 FTA <- round(rgamma(N_games, 5, 1),0)
 pts <- vector("numeric", length = N_games)
 
+set.seed(1471)
 for (i in seq_along(1:N_games)) { 
   
   pts[i] <-  sim_pts(FGA[i], FTA[i], hFG, hFT)
@@ -87,25 +36,29 @@ for (i in seq_along(1:N_games)) {
   }
 
 dat <- tibble(FGA, FTA, pts)  
-  
 
 ggplot(dat, aes(x = pts)) + 
   geom_histogram(fill = "#ff02ff", alpha = .5, color = "#ff02ff", bins = 30) + 
   labs(x = "PTS", 
-       y = "Frequency") + 
-  dark_theme()
+       y = "Frequency") #+ 
+#  dark_theme()
+#ggsave("materials/plots/session_08_simulation_shaq_points.png", height = 5, width = 6)
+
 
 m1_shaq <- quap(
   alist(
     pts ~ dnorm(mu, sd), 
-    mu <- a + b_1 * FGA + b_2 * FTA,
+    mu <- a + b_1 * FGA * 2 + b_2 * FTA,
     a ~ dunif(0,10),
-    b_1 ~ dunif(0, 3),
+    b_1 ~ dunif(0, 1),
     b_2 ~ dunif(0, 1),
-    sd ~ dunif(0,10)
+    sd ~ dunif(0,8)
   ),
   data = dat)
 precis(m1_shaq)
+
+plot(m1_shaq)
+pairs(m1_shaq, pars = c("a", "b_1", "b_2", "sd"))
 
 ## mean-centering
 
@@ -115,106 +68,105 @@ FTA_bar <- round(mean(dat$FTA),0)
 m2_shaq <- quap(
   alist(
     pts ~ dnorm(mu, sd), 
-    mu <- a + b_1 * (FGA-FGA_bar) + b_2 * (FTA-FTA_bar), 
+    mu <- a + b_1 * (FGA-FGA_bar) * 2 + b_2 * (FTA-FTA_bar), 
     a ~ dnorm(20,8), 
     b_1 ~ dunif(0, 3),
     b_2 ~ dunif(0, 1),
-    sd ~ dunif(0,10) 
+    sd ~ dunif(0,8) 
   ),
   data = dat)
 precis(m2_shaq)
 
-m3_shaq <- quap(
-  alist(
-    pts ~ dnorm(mu, sd),
-    mu <- a + b_1 * (FGA-FGA_bar) * 2 + b_2 * (FTA-FTA_bar), 
-    a ~ dnorm(20,8),
-    b_1 ~ dunif(0, 1),
-    b_2 ~ dunif(0, 1),
-    sd ~ dunif(0,10)
-  ),
-  data = dat)
-precis(m3_shaq)
-
+plot(m2_shaq)
+pairs(m2_shaq, pars = c("a", "b_1", "b_2", "sd"))
 
 
 # load and prepare data 
 
+
+# Pipe --------------------------------------------------------------------
+
 shaq <- read_csv("data/shaq.csv")
-dat <- list(FGA = shaq$FGA,
-            FTA = shaq$FTA, 
-            PTS = shaq$PTS,
-            Min = shaq$Minutes)
-
-
-# mediation / pipe
+dat <- list(FGA = shaq$FGA ,
+            FTA = shaq$FTA , 
+            PTS = shaq$PTS ,
+            MIN = shaq$Minutes ,
+            FTA_bar = round(mean(shaq$FTA),0) , 
+            FGA_bar = round(mean(shaq$FGA),0) , 
+            MIN_bar = round(mean(shaq$FGA),0))
 
 # without mediator
 
 ggplot(shaq, aes(x = Minutes, y = PTS)) + 
-  geom_point(size = 3, color =  "#ff02ff", alpha = .2) +
-  geom_smooth(method = "lm", color = "#FDB927") +
+  geom_point(size = 2, color =  "#ff02ff", alpha = .5) +
   labs(x = "Minutes", 
-       y = "Points") + 
-  dark_theme()
+       y = "Points") # + 
+#  dark_theme()
+#ggsave("materials/plots/session_08_data_shaq_points_min.png", height = 5, width = 6)
 
-Min_bar <- round(mean(dat$Min),0)
-m5_shaq <- quap(
+m3_shaq <- quap(
   alist(
     PTS ~ dnorm(mu, sd),
-    mu <- a + b_1 * (Min - Min_bar),
+    mu <- a + b_1 * (MIN - MIN_bar),
     a ~ dnorm(20, 8),
     b_1 ~ dunif(0, 2), 
-    sd ~ dunif(0,10) 
+    sd ~ dunif(0,8) 
   ),
   data = dat)
-precis(m5_shaq)
+precis(m3_shaq)
 
 # with mediator
 
-m6_shaq <- quap(
+m4_shaq <- quap(
   alist(
     PTS ~ dnorm(mu, sd), 
-    mu <- a + b_1 * (Min - Min_bar) + b_2 * (FGA - FGA_bar) + b_3 * (FTA - FTA_bar),
+    mu <- a + b_1 * (MIN- MIN_bar) + b_2 * (FGA - FGA_bar) + b_3 * (FTA - FTA_bar),
     a ~ dnorm(20, 8),
     b_1 ~ dnorm(0, 2), 
     b_2 ~ dunif(0, 2), 
     b_3 ~ dunif(0, 1), 
-    sd ~ dunif(0,10)
+    sd ~ dunif(0,8)
   ),
   data = dat)
-precis(m6_shaq)
+precis(m4_shaq)
 
-# Fork
-ggplot(diamonds, aes(y = price, x = clarity, color = clarity)) + 
-  geom_boxplot() +
-  theme_minimal()
 
-ggplot(diamonds, aes(y = price, x = cut, color = cut)) + 
-  geom_boxplot() +
-  theme_minimal()
 
-ggplot(diamonds, aes(y = price, x = color, color = color)) + 
-  geom_boxplot() +
-  theme_minimal()
+# Fork --------------------------------------------------------------------
 
-diamonds2 <- diamonds
-diamonds2$clarity <- fct_recode(factor(diamonds2$clarity),
-                           "1" = "I1", 
-                           "2" = "SI2", 
-                          "3" = "SI1", 
-                          "4" = "VS2", 
-                          "5" = "VS1", 
-                          "6" = "VVS2", 
-                          "7" = "VVS1", 
-                          "8" = "IF")
-diamonds2$clarity <- as.numeric(diamonds2$clarity)
-cor(diamonds2$price, diamonds2$clarity)
+diamonds$clarity <- as.numeric(diamonds$clarity)
+diamonds$cut <- as.numeric(diamonds$cut)
+diamonds$color <- as.numeric(diamonds$color)
+
+diamonds
+ggplot(diamonds, aes(y = price, x = as.factor(clarity))) + 
+  geom_violin(width=1.4, color = "#ff02ff", fill = "#ff02ff", alpha = .5) +
+  geom_boxplot(width=0.05, color="white", alpha=0.2) #+
+#  dark_theme()
+#ggsave("materials/plots/session_08_data_diamonds_price_clarity.png", height = 5, width = 8)
+
+ggplot(diamonds, aes(y = price, x = as.factor(cut))) + 
+  geom_violin(width=1.4, color = "green", fill = "green", alpha = .5) +
+  geom_boxplot(width=0.05, color="white", alpha=0.2) #+
+#  dark_theme()
+#ggsave("materials/plots/session_08_data_diamonds_price_cut.png", height = 5, width = 8)
+
+ggplot(diamonds, aes(y = price, x = as.factor(color*-1))) + 
+  geom_violin(width=1.4, color = "white", fill = "white", alpha = .5) +
+  geom_boxplot(width=0.05, color="white", alpha=0.2) # +
+#  dark_theme()
+#ggsave("materials/plots/session_08_data_diamonds_price_color.png", height = 5, width = 8)
+
+ggplot(diamonds, aes(y = price, x = carat)) + 
+  geom_jitter(color = "#ff02ff", fill = "#ff02ff", alpha = .1) # +
+#  dark_theme()
+#ggsave("materials/plots/session_08_data_diamonds_price_carat.png", height = 5, width = 8)
+
 
 dat <- list(
-  p = standardize(diamonds2$price), 
-  cl = standardize(diamonds2$clarity),
-  ca = standardize(diamonds2$carat)
+  p = standardize(diamonds$price), 
+  cl = standardize(diamonds$clarity),
+  ca = standardize(diamonds$carat)
 )
 
 m1 <- quap(
@@ -226,11 +178,10 @@ m1 <- quap(
     sd ~ dexp(1) 
   ),
   data = dat)
+
+
 precis(m1)
 
-ggplot(diamonds, aes(y = price, x = carat, color = clarity)) + 
-  geom_point(alpha = .5) +
-  theme_minimal()
 
 m2 <- quap(
   alist(
@@ -241,20 +192,9 @@ m2 <- quap(
     sd ~ dexp(1)
   ),
   data = dat)
+
+
 precis(m2)
-
-
-ggplot(diamonds, aes(y = carat, x = clarity, color = clarity)) + 
-  geom_boxplot() +
-  theme_minimal()
-
-ggplot(diamonds, aes(y = carat, x = cut, color = cut)) + 
-  geom_boxplot() +
-  theme_minimal()
-
-ggplot(diamonds, aes(y = carat, x = color, color = color)) + 
-  geom_boxplot() +
-  theme_minimal()
 
 
 m3 <- quap(
@@ -265,6 +205,8 @@ m3 <- quap(
     b ~ dnorm(0, .5), 
     sd ~ dexp(1)),
   data = dat)
+
+
 precis(m3)
 
 
@@ -278,4 +220,6 @@ m4 <- quap(
     sd ~ dexp(1)
   ),
   data = dat)
+
+
 precis(m4)
